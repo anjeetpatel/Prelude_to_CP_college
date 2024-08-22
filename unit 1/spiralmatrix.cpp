6c02a03ebb247248f72aad992baf99252030800a
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-22 14:51:29 +0530

    unit1

--- unit 1/spiralmatrix.cpp
+++ unit 1/spiralmatrix.cpp
@@ -1,37 +1,56 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <vector>
 
-void print_spiral(vector<vector<int>> &v, int r, int c){
+void print_spiral(std::vector<std::vector<int>> &v, int r, int c)
+{
     int top = 0;
     int bottom = r - 1;
     int left = 0;
     int right = c - 1;
-    while(top <= bottom && left <= right){
+    while (top <= bottom && left <= right)
+    {
         // Print from left to right
-        for(int i = left; i <= right; i++){
-            cout << v[top][i] << " ";
+        for (int i = left; i <= right; i++)
+        {
+            std::cout << v[top][i] << " ";
+        }
+        top++;
+
+        // Print from top to bottom
+        if (top <= bottom)
+        {
+            for (int i = top; i <= bottom; i++)
+            {
+                std::cout << v[i][right] << " ";
             }
-            top++;
+            right--;
+        }
+
+        // Print from right to left
+        if (left <= right)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                std::cout << v[bottom][i] << " ";
             }
-            // Print from top to bottom
-            for(int i = top; i <= bottom; i++){
-                cout << v[i][right] << " ";
-                }
-                right--;
-                }
-                // Print from right to left
-                for(int i = right; i >= left; i--){
-                    cout << v[bottom][i] << " ";
-                    }
-                    bottom--;
-                    }
-                    // Print from bottom to top
-                    for(int i = bottom; i >= top; i--){
-                        cout << v[i][left] << " ";
-                        }
-                        
+            bottom--;
+        }
+
+        // Print from bottom to top
+        if (top <= bottom)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                std::cout << v[i][left] << " ";
+            }
+            left++;
+        }
+    }
 }
 
-int main(){
-
+int main()
+{
+    std::vector<std::vector<int>> a = {{2, 3, 4, 5}, {5, 6, 7, 8}, {8, 9, 5, 4}, {2, 5, 7, 9}};
+    print_spiral(a, a.size(), a[0].size());
+    return 0;
 }

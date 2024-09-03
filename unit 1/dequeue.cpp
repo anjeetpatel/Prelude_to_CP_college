8d02a1f143e569e9841bb13a324e2ae5127f09da
anjeetpatel <anjeetpatel0306@gmail.com>
2024-09-03 20:42:30 +0530

    dequeue

---
+++ unit 1/dequeue.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node next;
+};
+
+void print_reverse(node first)
+{
+    if (first == NULL)
+        return;
+
+    print_reverse(first->next);
+    cout << first->data << " ";
+}
+
+void print_queue(struct node *front)
+{
+    if (front == NULL)
+        return;
+    node temp = front;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void dequeue(node &front, node *&rear)
+{
+    if (front == NULL)
+        return;
+    node *temp = front;
+    front = front->next;
+    delete temp;
+}
+
+void enqueue(struct node *&front, struct node *&rear, int key)
+{
+    node temp = new node;
+    temp->data = key;
+    temp->next = NULL;
+    if (front == NULL)
+    {
+        front = rear = temp;
+        return;
+    }
+    rear->next = temp;
+    rear = rear->next;
+}
+
+void reverse_queue(node &front, node *&rear)
+{
+    if (front == NULL)
+        return;
+
+    node *temp1 = NULL;
+    node *temp2 = front;
+
+    rear = front;
+
+    while (temp2 != NULL)
+    {
+        node *temp3 = temp2->next;
+        temp2->next = temp1;
+        temp1 = temp2;
+        temp2 = temp3;
+    }
+    front = temp1;
+}
+
+int main()
+{
+    node *front = NULL, *rear = NULL;
+    enqueue(front, rear, 2);
+    enqueue(front, rear, 3);
+    enqueue(front, rear, 4);
+    enqueue(front, rear, 5);
+    enqueue(front, rear, 6);
+
+    reverse_queue(front, rear);
+    print_queue(front);
+}

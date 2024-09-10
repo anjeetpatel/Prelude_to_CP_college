63eeebc22307a1fa8d8951ac9aaf8ad5aaa9f1c5
anjeetpatel <anjeetpatel0306@gmail.com>
2024-09-10 23:52:42 +0530

    gcd

--- unit 1/gcd.cpp
+++ unit 1/gcd.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
+int extendedeuclid(int a, int b){
+    if(a==0){
+        *x=0;
+        *y=1;
+        return b;
+    }
+    int x1, y1;
     
+    int gcd = extendedeuclid(b%a,a,&x1,&y1);
+      
+}
+int main(){
+    int x,y;
+    cout<<extendedeuclid(25,35,&x,&y);
 }

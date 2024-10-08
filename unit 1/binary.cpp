4fc817d00c33325fd8a3bbb5d2c14777005d3e12
anjeetpatel <anjeetpatel0306@gmail.com>
2024-10-08 11:48:00 +0530

    binary

--- unit 1/binary.cpp
+++ unit 1/binary.cpp
@@ -4,25 +4,65 @@ using namespace std;
 
 int binarysearch(int arr[], int low, int high, int key)
 {
-    while (low < high)
+    while (low <= high)
     {
         int mid = low + (high - low) / 2;
 
-        if (key == arr[mid])
+        if (arr[mid] == key)
             return mid;
+
         else if (key < arr[mid])
-            return binarysearch(arr, low, mid - 1, key);
-        else if (key > arr[mid])
-            return binarysearch(arr, mid + 1, high, key);
+            high = mid - 1;
+
+        else
+            low = mid + 1;
     }
     return -1;
-    
+}
+
+int findingpivot(int arr[], int low, int high)
+{
+    while (low <= high)
+    {
+        if (arr[low] <= arr[high])
+            return low;
+
+        int mid = (low + high) / 2;
+
+        if (mid > 0 && arr[mid] < arr[mid - 1])
+            return mid;
+
+        if (arr[mid] >= arr[low])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return 0;
+}
+
+int pivotedbinarysearch(int arr[], int low, int high, int key)
+{
+    int pivot = findingpivot(arr, low, high);
+
+    if (pivot == 0)
+        return binarysearch(arr, 0, high, key);
+
+    if (key == arr[pivot])
+        return pivot;
+
+    if (key >= arr[low])
+        return binarysearch(arr, low, pivot - 1, key);
+
+    return binarysearch(arr, pivot + 1, high, key);
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[] = {12, 0};
+
     int size = sizeof(arr) / sizeof(int);
-    int index = binarysearch(arr, 0, size - 1, 10);
-    cout << "Element found at index: " << index << endl;
+
+    int index = pivotedbinarysearch(arr, 0, size - 1, 12);
+
+    cout << index;
 }
